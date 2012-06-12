8c409f76e1faeca423692ac41f85f5c89662ada8
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-06-12 14:41:22 -0300

    debuging the oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -132,6 +132,7 @@ class User < ActiveRecord::Base
 
   def self.create_with_omniauth(auth)
     create! do |user|
+      raise auth.inspect
       user.provider = auth["provider"]
       user.uid = auth["uid"]
       user.name = auth["user_info"]["name"]
