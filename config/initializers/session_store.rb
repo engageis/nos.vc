f2c904f18e48917200e18c28d5449d5449b2ca0e
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-06-12 15:52:30 -0300

    changed the sesseion store key and secret token

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Catarse::Application.config.session_store :cookie_store, :key => '_catarse_session'
+Catarse::Application.config.session_store :cookie_store, :key => '_nosvc_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
