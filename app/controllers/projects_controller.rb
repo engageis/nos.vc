f9ee0be298726fa31961acf9497eb06cf3bdeb07
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-06-20 17:35:06 -0300

    changed the bitly api key (testing)

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -188,7 +188,7 @@ class ProjectsController < ApplicationController
   def bitly
     return unless Rails.env.production?
     require 'net/http'
-    res = Net::HTTP.start("api.bit.ly", 80) { |http| http.get("/v3/shorten?login=diogob&apiKey=R_5fe82290be618f624100373d35e8c75e&longUrl=#{CGI.escape(project_url(@project))}") }
+    res = Net::HTTP.start("api.bit.ly", 80) { |http| http.get("/v3/shorten?login=diogob&apiKey=R_76ee3ab860d76d0d1c1c8e9cc5485ca1&longUrl=#{CGI.escape(project_url(@project))}") }
     data = JSON.parse(res.body)['data']
     data['url'] if data
   end
