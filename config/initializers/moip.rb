2f8d01b407c2faae1a2f95d7e59dfcd8c8107379
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-06-21 17:22:28 -0300

    added moip uri for sandbox

--- config/initializers/moip.rb
+++ config/initializers/moip.rb
@@ -1,4 +1,7 @@
 MoIP.setup do |config|
   config.token = Configuration[:moip_token] or ''
   config.key = Configuration[:moip_key] or ''
+
+  # Temp
+  config.uri = "https://desenvolvedor.moip.com.br/sandbox"
 end
