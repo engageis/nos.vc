fa9ea101a10a2e28cc19ee5903adb2ee4338e154
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-06-21 16:02:49 -0300

    removed prints on assets initializer

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,27 +1,27 @@
 puts "removing existing symbolic links for assets"
 
-puts "    removing the fonts link..."
+#puts "    removing the fonts link..."
 system("rm -f #{Rails.root}/public/fonts")
-puts "    OK!"
+#puts "    OK!"
 
-puts "    removing the images link..."
+#puts "    removing the images link..."
 system("rm -f #{Rails.root}/public/images")
-puts "    OK!"
+#puts "    OK!"
 
-puts "    removing the javascripts link..."
+#puts "    removing the javascripts link..."
 system("rm -f #{Rails.root}/public/javascripts")
-puts "    OK!"
+#puts "    OK!"
 
 puts "creating the symbolic link for assets"
 
-puts "    creating the symbolic link for the fonts..."
+#puts "    creating the symbolic link for the fonts..."
 system("ln -s #{Rails.root}/app/assets/fonts #{Rails.root}/public/fonts")
-puts "    OK!"
+#puts "    OK!"
 
-puts "    creating the symbolic link for the images..."
+#puts "    creating the symbolic link for the images..."
 system("ln -s #{Rails.root}/app/assets/images #{Rails.root}/public/images")
-puts "    OK!"
+#puts "    OK!"
 
-puts "    creating the symbolic link for the javascripts..."
+#puts "    creating the symbolic link for the javascripts..."
 system("ln -s #{Rails.root}/app/assets/javascripts #{Rails.root}/public/javascripts")
-puts "    OK!"
+#puts "    OK!"
