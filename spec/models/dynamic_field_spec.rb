0c155e86309fe2cd4437db86e11442a97bf66674
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-08-22 14:18:21 -0300

    added association between dynamic_fields and dynamic_values, refactored the spec

--- spec/models/dynamic_field_spec.rb
+++ spec/models/dynamic_field_spec.rb
@@ -4,7 +4,13 @@ describe DynamicField do
   describe "validations" do
     subject { Factory(:dynamic_field) }
 
-    it { should belong_to(:project) }
-    it{ should validate_presence_of :input_name }
+    it { should validate_presence_of :input_name }
    end
+
+  describe "associations" do
+    subject { Factory(:dynamic_field) }
+
+    it { should belong_to(:project) }
+    it { should have_many(:dynamic_values) }
+  end
 end
