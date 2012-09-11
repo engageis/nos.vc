faef2f180514e2b486854fa35ce94ea5e7df58de
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-09-11 14:28:05 -0300

    attr for dynamic fields

--- app/models/dynamic_field.rb
+++ app/models/dynamic_field.rb
@@ -1,6 +1,6 @@
 class DynamicField < ActiveRecord::Base
   belongs_to :project
   has_many :dynamic_values
-  attr_accessible :input_name, :input_value, :required
+  attr_accessible :input_name, :input_value, :required, :project, :project_id
   validates :input_name, presence: true
 end
