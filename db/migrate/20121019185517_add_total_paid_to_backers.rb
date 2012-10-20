a3fa1cef113a36ef89d6f463143c5acd225c5d63
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-10-20 10:41:03 -0300

    migration typo error

--- db/migrate/20121019185517_add_total_paid_to_backers.rb
+++ db/migrate/20121019185517_add_total_paid_to_backers.rb
@@ -1,5 +1,5 @@
 class AddTotalPaidToBackers < ActiveRecord::Migration
   def change
-    add_column :backers, :total_paid, :decimal, :defaul => 0
+    add_column :backers, :total_paid, :decimal, :default => 0
   end
 end
