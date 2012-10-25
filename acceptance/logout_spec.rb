4f2877646f0d9148e5a64983629dab479ec1bef5
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-10-25 18:37:02 -0200

    making the logout acceptance spec pass

--- acceptance/logout_spec.rb
+++ acceptance/logout_spec.rb
@@ -1,3 +1,4 @@
+# Encoding: utf-8
 require File.expand_path(File.dirname(__FILE__) + '/acceptance_helper')
 
 feature "Logout Feature" do
@@ -5,10 +6,10 @@ feature "Logout Feature" do
   scenario "Given I'm logged in, I must be able to logout" do
 
     fake_login
-    page.should have_link(I18n.t('layouts.header.account'))
-    click_link I18n.t('layouts.header.account')
+    page.should have_link("Olá, #{current_user.display_name.split(' ').first}")
+    click_link "Olá, #{current_user.display_name.split(' ').first}"
     click_link "Sair"
-    page.should have_no_link(I18n.t('layouts.header.account'))
+    page.should have_no_link("Olá, #{current_user.display_name.split(' ').first}")
     verify_translations
 
   end
