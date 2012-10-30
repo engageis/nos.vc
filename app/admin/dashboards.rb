2f8e3f83b63e3c8c30f2c8e2bdc66c52c06fc13a
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-10-30 13:44:52 -0200

    removed the deprecattion warning from active admin dashboard

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -1,6 +1,5 @@
 #encoding: utf-8
 ActiveAdmin.register_page "Dashboard" do
-
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
