e215022ebecda7b11aac3458b0ee86ee744b483e
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-10-30 13:44:42 -0200

    making the start project acceptance spec pass

--- acceptance/start_project_spec.rb
+++ acceptance/start_project_spec.rb
@@ -6,9 +6,9 @@ feature "Send Project Feature", :driver => :selenium do
 
   scenario "I'm not logged in and I want to send a project. It should ask for login." do
     visit homepage
-    click_link 'envie'
+    click_link 'Criar um encontro'
     verify_translations
-    current_path.should == guidelines_path
+    current_path.should == login_path
   end
 
   scenario "I am logged in and I want to send a project" do
@@ -16,46 +16,27 @@ feature "Send Project Feature", :driver => :selenium do
     c = Factory(:category)
     visit homepage
     fake_login
-    click_link 'envie'
+    click_link 'Criar um encontro'
     verify_translations
-    current_path.should == guidelines_path
-
-    within 'head title' do
-      page.should have_content("Como funciona")
-    end
-
-    within '.title' do
-      within 'h1' do
-        page.should have_content("Como funciona")
-      end
-    end
-
-    uncheck 'accept'
-    find_button('Quero enviar meu projeto')['disabled'].should == 'true'
-    check 'accept'
-    find_button('Quero enviar meu projeto')['disabled'].should be_nil
-    click_button 'Quero enviar meu projeto'
-
-    sleep 2
-    verify_translations
-
-    within '.title' do
-      within 'h1' do
-        page.should have_content("Envie seu projeto")
-      end
-    end
-
     current_path.should == start_projects_path
 
     within '.bootstrap-form' do
-      fill_in 'how_much_you_need', with: 10
+      fill_in  'name', :with => 'Foo bar'
       fill_in 'about', :with => 'about this very cool project'
-      fill_in 'rewards', :with => 'rewards of this very cool project'
-      fill_in 'links', :with => 'links of this very cool project'
+      fill_in 'when', :with => "today"
+      fill_in 'where', :with => "here"
+      fill_in 'video', :with => "http://vimeo.com/28220980"
+      fill_in 'leader_name', :with => "me"
+      fill_in 'more_info', :with => "on my site"
+      fill_in 'goal', :with => 10
+      fill_in 'maximum_backers', :with => 10
+      fill_in 'how_works', :with => "Foo bar"
+      fill_in 'know_us_via', :with => 'goole'
+
       fill_in 'contact', :with => 'foo@bar.com'
       check 'accept'
       verify_translations
-      click_button 'Enviar o projeto'
+      click_button 'Enviar'
     end
 
     ActionMailer::Base.deliveries.should_not be_empty
