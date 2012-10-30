ed2682f1b1d4adc46a0418892b6dd739c6e9fc58
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-10-30 12:34:10 -0200

    making the pending project acceptance spec pass

--- acceptance/pending_projects_spec.rb
+++ acceptance/pending_projects_spec.rb
@@ -17,10 +17,9 @@ feature "Pending projects" do
   scenario "with admin user" do
     user_to_admin(current_user)
     visit pending_projects_path
-    verify_translations
     page.should have_no_css('.failure.wrapper')
     within ".title" do
-      page.should have_css('h1', :text => 'Gerenciamento dos projetos')
+      page.should have_css('h1', :text => 'Gerenciamento dos encontros')
     end
   end
 
@@ -28,8 +27,7 @@ feature "Pending projects" do
     add_some_projects(10)
     user_to_admin(current_user)
     visit pending_projects_path
-    verify_translations
-    page.should have_content "Encontrados 10 projetos"
+    page.should have_content "Encontrados 10 encontros"
     all("#pending_projects table tbody tr").should have(10).itens
   end
 
@@ -37,16 +35,13 @@ feature "Pending projects" do
     add_some_projects(1)
     user_to_admin(current_user)
     visit homepage
-    verify_translations
     page.should have_no_css('.project_list_header')
     visit pending_projects_path
     check 'project__visible__1'
-    verify_translations
     # Had to add this sleep to wait for ajax to update the records
     sleep 2
     visit homepage
-    verify_translations
-    page.should have_css('.title h2', :text => "novos e fresquinhos")
+    page.should have_content("Saindo do forno")
     page.should have_css('.recents_projects')
     all('.recents_projects .projects .curated_project').should have(1).item
     within '.recents_projects' do
