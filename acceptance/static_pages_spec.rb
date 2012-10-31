085e62340619864282fd06e58875cdbc050a5ece
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-10-31 11:19:22 -0200

    making the static acceptance spec pass and created more

--- acceptance/static_pages_spec.rb
+++ acceptance/static_pages_spec.rb
@@ -5,45 +5,21 @@ require File.expand_path(File.dirname(__FILE__) + '/acceptance_helper')
 feature "Static Pages Feature", :driver => :selenium do
 
   scenario "I visit the guidelines page" do
-
     visit homepage
     click_link 'Como funciona'
     verify_translations
     current_path.should == guidelines_path
 
     within 'head title' do
-      page.should have_content("Como funciona o Catarse?")
+      page.should have_content("Como funciona")
     end
 
     within '.title' do
-      within 'h1' do
-        page.should have_content("Como funciona o Catarse?")
-      end
+      page.should have_content("Aprenda e ensine ao lado de pessoas apaixonadas pelas mesmas coisas que você.")
     end
-
-  end
-
-  scenario "I visit the FAQ page" do
-
-    visit homepage
-    click_link 'F.A.Q./Ajuda'
-    verify_translations
-    current_path.should == faq_path
-
-    within 'head title' do
-      page.should have_content("Perguntas frequentes")
-    end
-
-    within '.title' do
-      within 'h1' do
-        page.should have_content("Perguntas frequentes")
-      end
-    end
-
   end
 
   scenario "I visit the terms of use page" do
-
     visit homepage
     click_link 'Termos de Uso'
     verify_translations
@@ -54,15 +30,11 @@ feature "Static Pages Feature", :driver => :selenium do
     end
 
     within '.title' do
-      within 'h1' do
-        page.should have_content("Termos de uso")
-      end
+      page.should have_content("Termos de uso")
     end
-
   end
 
   scenario "I visit the privacy policy page" do
-
     visit homepage
     click_link 'Política de Privacidade'
     verify_translations
@@ -73,11 +45,38 @@ feature "Static Pages Feature", :driver => :selenium do
     end
 
     within '.title' do
-      within 'h1' do
-        page.should have_content("Política de privacidade")
-      end
+      page.should have_content("Política de privacidade")
+    end
+  end
+
+  scenario "I visit the about page" do
+    visit homepage
+    click_link 'Sobre'
+    verify_translations
+    current_path.should == about_us_path
+
+    within 'head title' do
+      page.should have_content("Sobre Nós.vc")
     end
 
+    within '.title' do
+      page.should have_content("Acreditamos que aprendizado tem que ser um processo inspirador.")
+    end
+  end
+
+  scenario "I visit the contact page" do
+    visit homepage
+    click_link 'Contato'
+    verify_translations
+    current_path.should == contact_path
+
+    within 'head title' do
+      page.should have_content("Contato")
+    end
+
+    within '.title' do
+      page.should have_content("Vem falar com a gente")
+    end
   end
 
 end
