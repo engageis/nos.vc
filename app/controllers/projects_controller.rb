402ac51c4537e587b4c4bff38fba14b56046aee6
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-04-11 11:35:04 -0300

    On project create redirect to root path

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -77,7 +77,9 @@ class ProjectsController < ApplicationController
     params[:project][:expires_at] += (23.hours + 59.minutes + 59.seconds) if params[:project][:expires_at]
     validate_rewards_attributes if params[:project][:rewards_attributes].present?
     params[:project].delete(:dynamic_fields_attributes) unless params[:has_dynamic_fields]
-    create!(:notice => t('projects.create.success'))
+    create!{ |format|
+      format.html { redirect_to root_path, :notice => t('projects.create.success') }
+    }
     # When it can't create the project the @project doesn't exist and then it causes a record not found
     # because @project.reload *works only with created records*
     unless @project.new_record?
