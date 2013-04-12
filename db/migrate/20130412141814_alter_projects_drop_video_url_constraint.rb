34be71ea1a7cb7bca69c835a4a85708b47f51e18
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-04-12 13:00:32 -0300

    Now the fuck*** migration run on heroku

--- db/migrate/20130412141814_alter_projects_drop_video_url_constraint.rb
+++ db/migrate/20130412141814_alter_projects_drop_video_url_constraint.rb
@@ -1,15 +1,10 @@
 require 'sexy_pg_constraints'
 class AlterProjectsDropVideoUrlConstraint < ActiveRecord::Migration
   def up
-    execute "
-    ALTER TABLE projects ALTER video_url DROP NOT NULL;
-    ALTER TABLE projects DROP CONSTRAINT projects_video_url_not_blank;
-    "
+    execute "ALTER TABLE projects ALTER video_url DROP NOT NULL;"
   end
 
   def down
-    execute "
-    ALTER TABLE projects ALTER video_url SET NOT NULL;
-    "
+    execute "ALTER TABLE projects ALTER video_url SET NOT NULL;"
   end
 end
