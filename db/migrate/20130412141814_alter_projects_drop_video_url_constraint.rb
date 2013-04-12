525ffd874902b563b35ba06edb300a34eca3e854
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-04-12 12:44:28 -0300

    Trying to drop constraint on heroku again 2

--- db/migrate/20130412141814_alter_projects_drop_video_url_constraint.rb
+++ db/migrate/20130412141814_alter_projects_drop_video_url_constraint.rb
@@ -1,9 +1,6 @@
 require 'sexy_pg_constraints'
 class AlterProjectsDropVideoUrlConstraint < ActiveRecord::Migration
   def up
-    constrain :projects do |t|
-      t.video_url :not_blank => false
-    end
     execute "
     ALTER TABLE projects ALTER video_url DROP NOT NULL;
     ALTER TABLE projects DROP CONSTRAINT projects_video_url_not_blank;
@@ -11,12 +8,8 @@ class AlterProjectsDropVideoUrlConstraint < ActiveRecord::Migration
   end
 
   def down
-    constrain :projects do |t|
-      t.video_url :not_blank => true
-    end
     execute "
     ALTER TABLE projects ALTER video_url SET NOT NULL;
-    ALTER TABLE projects ADD  projects_video_url_not_blank CHECK(video_url <> '');
     "
   end
 end
