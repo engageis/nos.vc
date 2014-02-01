03cb6b863635e9050a69f109cb7b830863f0afda
Lucas Zawacki <lfzawacki@gmail.com>
2014-02-01 17:56:13 -0200

    Facebook omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,16 @@
 require 'omniauth-openid'
+require 'omniauth-facebook'
 require 'openid/store/filesystem'
 
-Rails.application.config.middleware.use OmniAuth::Builder do  
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
   use OmniAuth::Strategies::OpenID, :store => OpenID::Store::Filesystem.new("#{Rails.root}/tmp")
 
   provider :open_id, :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
+
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+
   begin
     OauthProvider.all.each do |p|
       # This hack can be removed after the upgrade to omniauth 2.0
