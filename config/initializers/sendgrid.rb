e90b7ffb77aa7e583a9d3e3dfdcc4635c0545490
Eduardo Cuducos <cuducos@gmail.com>
2015-10-30 17:42:27 +0000

    Fix SendGrid

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -4,9 +4,10 @@ begin
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => 'nosvc',
-    :password       =>  Configuration[:sendgrid],
-    :domain         => 'heroku.com'
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
     }
     ActionMailer::Base.delivery_method = :smtp
   end
