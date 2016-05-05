2fac5fba052949d6a212621bcbff2c1f0a0ffedd
Lucas Zawacki <lfzawacki@gmail.com>
2016-05-05 19:09:07 -0300

    Correct locale session errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -57,6 +57,7 @@ class ApplicationController < ActionController::Base
   def set_locale
     return unless params[:locale]
     I18n.locale = params[:locale]
+    session[:locale] = params[:locale]
     return unless current_user
     current_user.update_attribute :locale, params[:locale] if params[:locale] != current_user.locale
   end
@@ -66,11 +67,12 @@ class ApplicationController < ActionController::Base
     return if params[:locale]
     new_locale = current_user.locale if current_user
     new_locale = session[:locale] if session[:locale]
+
     unless new_locale
       new_locale = request.compatible_language_from(I18n.available_locales.map(&:to_s))
       new_locale = I18n.default_locale.to_s unless new_locale
-      #flash[:locale] = t('notify_locale', :locale => new_locale)
     end
+
     return redirect_to replace_locale(new_locale)
   end
 
