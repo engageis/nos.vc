dc218171f8baf88bad0d1bd3a2a69be6b9255fc3
Lucas Zawacki <lfzawacki@gmail.com>
2017-05-12 01:22:06 -0300

    Fix facebook app id

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -9,19 +9,4 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], :token_params => {
     :parse => :json
   }
-
-  begin
-    OauthProvider.all.each do |p|
-      # This hack can be removed after the upgrade to omniauth 2.0
-      # Where every provider will accept the options hash
-      unless p.scope.nil?
-        provider p.strategy, p.key, p.secret, {:scope => p.scope}
-      else
-        provider p.strategy, p.key, p.secret
-      end
-    end
-  rescue Exception => e
-    # We should initialize even with errors during providers setup
-    Rails.logger.error "Error while setting omniauth providers: #{e.inspect}"
-  end
 end
