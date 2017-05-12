60e1238d437c2a8e2b6f394fce8c75295d5115fb
Lucas Zawacki <lfzawacki@gmail.com>
2017-05-12 01:42:38 -0300

    Remove unused social networks login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,9 +2,8 @@ require 'omniauth-openid'
 require 'openid/store/filesystem'
 
 Rails.application.config.middleware.use OmniAuth::Builder do  
-  use OmniAuth::Strategies::OpenID, :store => OpenID::Store::Filesystem.new("#{Rails.root}/tmp")
-
-  provider :open_id, :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
+  # use OmniAuth::Strategies::OpenID, :store => OpenID::Store::Filesystem.new("#{Rails.root}/tmp")
+  # provider :open_id, :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
 
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], :token_params => {
     :parse => :json
